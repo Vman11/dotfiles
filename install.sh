c7dd699f6ec45e7c21ac51d255e69ee2737b1bc4
Vincenzo DiMatteo <47278634+Vman11@users.noreply.github.com>
2024-08-22 14:19:01 -0400

    wip

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ sudo apt install curl
 git config --global user.name "Vincenzo DiMatteo"
 git config --global user.email "47278634+Vman11@users.noreply.github.com"
 #ssh-keygen -t ed25519 -C "47278634+Vman11@users.noreply.github.com"
+
 ln -sf "$(realpath .bashrc)" "$HOME"/.bashrc
 ln -sf "$(realpath .terminator)" "$HOME"/.config/terminator/config
 #ls -al "$HOME"
 
 source "$HOME"/.bashrc
 
-curl https://pyenv.run | bash
-
+#curl https://pyenv.run | bash
+#curl -f https://zed.dev/install.sh | sh
 
 echo "
 Next Steps:
