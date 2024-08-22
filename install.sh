933f1dc73391e9cff4621e900ed954dff51c6522
Vincenzo DiMatteo <47278634+Vman11@users.noreply.github.com>
2024-08-22 13:09:10 -0400

    fixes, terminator added

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 #__doc__='
 #Explicitly setup symlinks
 #'
+sudo apt update && sudo apt upgrade
+sudo apt install terminator
+
+git config --global user.name "Vincenzo DiMatteo"
+git config --global user.email "47278634+Vman11@users.noreply.github.com"
 
 ln -sf "$(realpath .bashrc)" "$HOME"/.bashrc
+ln -sf "$(realpath .terminator)" "$HOME"/.config/terminator/config
 #ls -al "$HOME"
 
 source "$HOME"/.bashrc
+
+curl https://pyenv.run | bash
 
 
 echo "
